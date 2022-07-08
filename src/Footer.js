45fad56819351e08e85615f708a698f7842418cf
AleKoles <kolesnikova.alexandra@gmail.com>
2022-07-09 00:16:37 +0300

    rel="noreferrer" in footer

--- src/Footer.js
+++ src/Footer.js
@@ -6,7 +6,7 @@ export default function Footer() {
       <a
         target="_blank"
         className="nav-link"
-        rel="noopener"
+        rel="noreferrer"
         href="https://github.com/AleKoles/weather-react"
       >
         <i className="fab fa-github" aria-hidden="true"></i> Source Code
