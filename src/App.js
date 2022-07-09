099d4dd0e59704663d4b8037ca356a928dd206e0
AleKoles <kolesnikova.alexandra@gmail.com>
2022-07-09 11:29:11 +0300

    feat: added more weather date

--- src/App.js
+++ src/App.js
@@ -1,111 +1,209 @@
 import React, { useState } from "react";
 import "./App.css";
 import axios from "axios";
+import "bootstrap";
+
+let timeDate = {
+  date: "23",
+  month: "June",
+  day: "Wednesday",
+  hours: 17,
+  minutes: 30,
+};
 
 export default function App() {
-  let [temperature, setTemperature] = useState("");
-  let [city, setCity] = useState("Kyiv");
-  let [cityText, setCityText] = useState("Kyiv");
-  let [tempText, setTempText] = useState("");
+  const [city, setCity] = useState("");
+  const [loaded, setLoaded] = useState(false);
+  const [weather, setWeather] = useState({});
 
-  function showTemperature(responce) {
-    setTemperature(Math.round(responce.data.main.temp));
+  function displayWeather(response) {
+    setLoaded(true);
+    setWeather({
+      temperature: response.data.main.temp,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      description: response.data.weather[0].description,
+    });
   }
 
-  let apiKey = "3818ef8d96aa3cbe7d3ceb323fd21a5c";
-  let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(url).then(showTemperature);
-
-  function showText(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    setCityText(`${city}`);
-    setTempText(`${temperature}`);
+    let apiKey = "3818ef8d96aa3cbe7d3ceb323fd21a5c";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayWeather);
   }
 
   function updateCity(event) {
     setCity(event.target.value);
   }
 
-  let weatherData = {
-    description: "Cloudy",
-    imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
-    precipitation: 70,
-    wind: 7,
-  };
-
-  let timeDate = {
-    date: "23",
-    month: "June",
-    day: "Wednesday",
-    hours: 17,
-    minutes: 30,
-  };
-
-  return (
-    <div className="container">
-      <header>
-        <a
-          className="my-button border-0"
-          title="Current Location"
-          id="current-button"
-          href="/"
-        >
-          <i className="fa-solid fa-location-dot"></i>
-        </a>
-
-        <form className="d-flex" id="form" role="search" onSubmit={showText}>
-          <input
-            className="form-control me-2"
-            id="input-city"
-            type="search"
-            placeholder="City"
-            aria-label="Search"
-            onChange={updateCity}
-          />
-          <button className="btn search-button my-button" type="submit">
-            Search
-          </button>
-        </form>
-      </header>
-      <main>
-        <div className="top">
-          <div className="city-name">
-            <h1>{cityText}</h1>
-            <h2 className="temperature">
-              <strong>{tempText}</strong>
-              <span className="degrees">
-                <a href="/" id="celsius">
-                  <small>°C</small>
-                </a>{" "}
-                |
-                <a href="/" id="fahrenheit">
-                  <small>°F</small>
-                </a>
-              </span>
-            </h2>
-          </div>
-          <div className="pic">
-            <img src={weatherData.imgUrl} alt={weatherData.description} />
-          </div>
-        </div>
-
-        <div className="weather-info">
-          <div className="info-main">
-            <p>{weatherData.description}</p>
-            <p>Precipitation: {weatherData.precipitation} %</p>
-            <p>Wind: {weatherData.wind} km/h</p>
-          </div>
-          <div className="other-main">
-            <p>
-              {timeDate.month}, {timeDate.date}
-            </p>
-            <p>{timeDate.day}</p>
-            <p>
-              {timeDate.hours}:{timeDate.minutes}
-            </p>
-          </div>
-        </div>
-      </main>
-    </div>
+  let form = (
+    <form className="d-flex" id="form" role="search" onSubmit={handleSubmit}>
+      <input
+        className="form-control me-2"
+        id="input-city"
+        type="search"
+        placeholder="City"
+        aria-label="Search"
+        onChange={updateCity}
+      />
+      <button className="btn search-button my-button" type="submit">
+        Search
+      </button>
+    </form>
   );
+
+  if (loaded) {
+    return (
+      <div className="container">
+        <header>
+          <a
+            className="my-button border-0"
+            title="Current Location"
+            id="current-button"
+            href="/"
+          >
+            <i className="fa-solid fa-location-dot"></i>
+          </a>
+
+          {form}
+        </header>
+        <main>
+          <div className="top">
+            <div className="city-name">
+              <h1>{city}</h1>
+              <h2 className="temperature">
+                <strong>{Math.round(weather.temperature)}</strong>
+                <span className="degrees">
+                  <a href="/" id="celsius">
+                    <small>°C</small>
+                  </a>{" "}
+                  |
+                  <a href="/" id="fahrenheit">
+                    <small>°F</small>
+                  </a>
+                </span>
+              </h2>
+            </div>
+            <div className="pic">
+              <img src={weather.icon} alt={weather.description} />
+            </div>
+          </div>
+
+          <div className="weather-info">
+            <div className="info-main">
+              <p>{weather.description}</p>
+              <p>Humidity: {weather.humidity} %</p>
+              <p>Wind: {weather.wind} km/h</p>
+            </div>
+            <div className="other-main">
+              <p>
+                {timeDate.month}, {timeDate.date}
+              </p>
+              <p>{timeDate.day}</p>
+              <p>
+                {timeDate.hours}:{timeDate.minutes}
+              </p>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container">
+        <header>
+          <a
+            className="my-button border-0"
+            title="Current Location"
+            id="current-button"
+            href="/"
+          >
+            <i className="fa-solid fa-location-dot"></i>
+          </a>
+
+          {form}
+        </header>
+        <main>
+          <div className="top">
+            <div className="city-name">
+              <h1>Kyiv</h1>
+              <h2 className="temperature">
+                <strong>25</strong>
+                <span className="degrees">
+                  <a href="/" id="celsius">
+                    <small>°C</small>
+                  </a>{" "}
+                  |
+                  <a href="/" id="fahrenheit">
+                    <small>°F</small>
+                  </a>
+                </span>
+              </h2>
+            </div>
+            <div className="pic">
+              <img src={weather.icon} alt={weather.description} />
+            </div>
+          </div>
+
+          <div className="weather-info">
+            <div className="info-main">
+              <p>Sunny</p>
+              <p>Humidity: 40 %</p>
+              <p>Wind: 10 km/h</p>
+            </div>
+            <div className="other-main">
+              <p>
+                {timeDate.month}, {timeDate.date}
+              </p>
+              <p>{timeDate.day}</p>
+              <p>
+                {timeDate.hours}:{timeDate.minutes}
+              </p>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
+
+// export default function App() {
+//   let [temperature, setTemperature] = useState("");
+//   let [city, setCity] = useState("Kyiv");
+//   let [cityText, setCityText] = useState("Kyiv");
+//   let [tempText, setTempText] = useState("");
+
+//   function showTemperature(responce) {
+//     setTemperature(Math.round(responce.data.main.temp));
+//   }
+
+//   let apiKey = "3818ef8d96aa3cbe7d3ceb323fd21a5c";
+//   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+//   axios.get(url).then(showTemperature);
+
+//   function showText(event) {
+//     event.preventDefault();
+//     setCityText(`${city}`);
+//     setTempText(`${temperature}`);
+//   }
+
+//   function updateCity(event) {
+//     setCity(event.target.value);
+//   }
+
+//   let weatherData = {
+//     description: "Cloudy",
+//     imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+//     precipitation: 70,
+//     wind: 7,
+//   };
+
+//   let timeDate = {
+//     date: "23",
+//     month: "June",
+//     day: "Wednesday",
+//     hours: 17,
+//     minutes: 30,
+//   };
