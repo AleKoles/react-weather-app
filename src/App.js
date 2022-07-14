bcbb484ee61165edd8d571721b9cecbf53bbb853
AleKoles <kolesnikova.alexandra@gmail.com>
2022-07-14 23:26:04 +0300

    added default city when page loaded

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ export default function App() {
 
   function handleSubmit(event) {
     event.preventDefault();
+    search();
+  }
+
+  function search() {
     let apiKey = "3818ef8d96aa3cbe7d3ceb323fd21a5c";
     let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(displayWeather);
@@ -90,84 +94,11 @@ export default function App() {
             </div>
           </div>
         </main>
-      </div>
-    );
-  } else {
-    return (
-      <div className="container">
-        <header>
-          <a
-            className="my-button border-0"
-            title="Current Location"
-            id="current-button"
-            href="/"
-          >
-            <i className="fa-solid fa-location-dot"></i>
-          </a>
-
-          {form}
-        </header>
-        <main>
-          <div className="top">
-            <div className="city-name">
-              <h1>Kyiv</h1>
-              <Degrees celsius={weather.temperature} />
-            </div>
-            <div className="pic">
-              <img src={weather.icon} alt={weather.description} />
-            </div>
-          </div>
-
-          <div className="weather-info">
-            <div className="info-main">
-              <p class="text-capitalize">{weather.description}</p>
-              <p>Humidity: 40 %</p>
-              <p>Wind: 10 km/h</p>
-            </div>
-            {/* <TimeDate date={weather.date} /> */}
-          </div>
-        </main>
         <Footer />
       </div>
     );
+  } else {
+    search();
+    return "Loading...";
   }
 }
-
-// export default function App() {
-//   let [temperature, setTemperature] = useState("");
-//   let [city, setCity] = useState("Kyiv");
-//   let [cityText, setCityText] = useState("Kyiv");
-//   let [tempText, setTempText] = useState("");
-
-//   function showTemperature(responce) {
-//     setTemperature(Math.round(responce.data.main.temp));
-//   }
-
-//   let apiKey = "3818ef8d96aa3cbe7d3ceb323fd21a5c";
-//   let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-//   axios.get(url).then(showTemperature);
-
-//   function showText(event) {
-//     event.preventDefault();
-//     setCityText(`${city}`);
-//     setTempText(`${temperature}`);
-//   }
-
-//   function updateCity(event) {
-//     setCity(event.target.value);
-//   }
-
-//   let weatherData = {
-//     description: "Cloudy",
-//     imgUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
-//     precipitation: 70,
-//     wind: 7,
-//   };
-
-//   let timeDate = {
-//     date: "23",
-//     month: "June",
-//     day: "Wednesday",
-//     hours: 17,
-//     minutes: 30,
-//   };
